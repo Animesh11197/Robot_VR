4a793680d8fc3e6c6245834610dc00246a8b8365
tusadi <tpurang@yahoo.in>
2019-03-17 19:31:05 +0530

    First Commit

---
+++ Assets/ArmRig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRig : MonoBehaviour {
+
+	public GameObject platform, arm;
+	float hz, vt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		hz = Input.GetAxis ("Horizontal_arrow");
+		vt = Input.GetAxis ("Vertical_arrow");
+
+		platform.transform.Rotate (Vector3.up * hz*Time.deltaTime*360);
+		arm.transform.Rotate (Vector3.right*vt*Time.deltaTime*360);
+	}
+}
---
+++ Assets/CameraRig.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour {
+
+	public GameObject pole, platform; //pole = vertical axis, platoform = horizontal axis
+	float  hz, vt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hz = Input.GetAxis ("Horizontal");
+		vt = Input.GetAxis ("Vertical");
+
+		pole.transform.Rotate (Vector3.up * hz*Time.deltaTime*360);
+
+		platform.transform.Rotate (Vector3.right*-vt*Time.deltaTime*360);
+
+	}
+}
